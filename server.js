fa7f41d4a06fe05b788388ce8142924accb82951
Tom Chen <chentom77@gmail.com>
2017-02-13 22:59:23 -0700

    Make port overridable

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import Express from 'express'
     
 const app = Express()
-const port = 8080
+const port = process.env['PORT'] || '8080'
 
 app.use(Express.static('static'))
 app.use(Express.static('dist'))
 
-console.log("Starting react server on port", port)
+console.log("Starting application on port", port)
 app.listen(port)
 
 
--- src/app.js
+++ src/app.js
@@ -4,4 +4,4 @@ import HelloWorld from './component/hello_world'
 
 import '../styles/app.scss'
 
-ReactDOM.render(<HelloWorld/>, document.getElementById('container'))
+ReactDOM.render(<HelloWorld/>, document.getElementById('root'))
